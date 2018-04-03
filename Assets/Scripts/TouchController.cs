a5bdc0abd79f9106b5d3f6ec714599435200d763
arakij <arakij@uci.edu>
2018-04-03 13:57:45 -0700

    Added OVR library

---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour {
+    public OVRInput.Controller controller;
+    // Use this for initialization
+    void Start() {
+        transform.localPosition = OVRInput.GetLocalControllerPosition(controller);
+        transform.localRotation = OVRInput.GetLocalControllerRotation(controller);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
