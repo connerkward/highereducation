540ee48dcd28f9857bb1a7de8433091805d153c9
Archana <archana.sk19@gmail.com>
2018-05-28 17:12:59 -0700

    Dialogue Controller and Scene transitions
    Added placeholder dialogue files
    Added Scene controller to play the dialogues in order
    Added transition from Garden 2 to dream and back

---
+++ Assets/Scripts/DreamSceneController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DreamSceneController : MonoBehaviour {
+    public AudioSource source;
+    public AudioClip[] scene2p1Audio;
+    public AudioClip[] scene2p2Audio;
+    public AudioClip[] scene2p3Audio;
+    public AudioClip[] scene2p4Audio;
+
+    public float delay;
+    public float waitTime;
+    public Image overlayImage;
+    // Use this for initialization
+
+    private void Awake()
+    {
+        scene2p1Audio = Resources.LoadAll<AudioClip>("Audio/Recordings/scene2p1");
+        scene2p2Audio = Resources.LoadAll<AudioClip>("Audio/Recordings/scene2p2");
+        scene2p3Audio = Resources.LoadAll<AudioClip>("Audio/Recordings/scene2p3");
+        scene2p4Audio = Resources.LoadAll<AudioClip>("Audio/Recordings/scene2p4");
+    }
+
+    IEnumerator Start () {
+        Debug.Log("Loaded Garden Dream");
+        source = GetComponent<AudioSource>();
+
+        if (PlayerPrefs.GetInt("sceneNo") == 0)
+        {
+            PlayerPrefs.SetInt("sceneNo", 1);
+            yield return StartCoroutine(Scene2p1Events());
+            yield return StartCoroutine(Scene2p2Events());
+            yield return StartCoroutine(Scene2p3Events());
+            yield return StartCoroutine(Scene2p4Events());
+
+            float timer = waitTime;
+            Color c = overlayImage.color;
+            overlayImage.enabled = true;
+            while (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                overlayImage.color = new Color(c.r, c.b, c.g, 1f - timer / waitTime);
+                yield return new WaitForSeconds(Time.deltaTime);
+            }
+
+            SceneManager.LoadScene("Garden 2");
+        }
+    }
+
+    IEnumerator Scene2p1Events()
+    {
+        Debug.Log("scene2p1 Events");
+        foreach (AudioClip clip in scene2p1Audio)
+        {
+            yield return StartCoroutine(playDialogue(clip));
+        } 
+        yield return null;
+    }
+
+    IEnumerator Scene2p2Events()
+    {
+        Debug.Log("scene2p2 Events");
+        foreach (AudioClip clip in scene2p2Audio)
+        {
+            yield return StartCoroutine(playDialogue(clip));
+        }
+    }
+
+    IEnumerator Scene2p3Events()
+    {
+        Debug.Log("scene2p3 Events");
+        foreach (AudioClip clip in scene2p3Audio)
+        {
+            yield return StartCoroutine(playDialogue(clip));
+        }
+        yield return null;
+    }
+
+    IEnumerator Scene2p4Events()
+    {
+        Debug.Log("scene2p4 Events");
+        yield return StartCoroutine(playDialogue(scene2p4Audio[0]));
+        if (ControllerInputHandler.instance.speed<0.5)
+        {
+            yield return StartCoroutine(playDialogue(scene2p4Audio[1]));
+            yield return StartCoroutine(playDialogue(scene2p4Audio[2]));
+            yield return StartCoroutine(playDialogue(scene2p4Audio[3]));
+        }
+        else
+        {
+            yield return StartCoroutine(playDialogue(scene2p4Audio[4]));
+            yield return StartCoroutine(playDialogue(scene2p4Audio[5]));
+            yield return StartCoroutine(playDialogue(scene2p4Audio[6]));
+        }
+        yield return null;
+    }
+
+    IEnumerator playDialogue(AudioClip clip)
+    {
+        Debug.Log(clip.name);
+        source.PlayOneShot(clip);
+        yield return new WaitForSeconds(clip.length+1);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour {
+    public AudioSource source;
+    public AudioClip[] scene1Audio;
+    public AudioClip[] scene3Audio;
+    public AudioClip[] scene4Audio;
+
+    public float delay;
+    public float waitTime;
+    public Image overlayImage;
+    // Use this for initialization
+
+    private void Awake()
+    {
+        scene1Audio = Resources.LoadAll<AudioClip>("Audio/Recordings/scene1");
+        scene3Audio = Resources.LoadAll<AudioClip>("Audio/Recordings/scene3");
+        scene4Audio = Resources.LoadAll<AudioClip>("Audio/Recordings/scene4");
+    }
+    IEnumerator Start () {
+        Debug.Log("scenecontroller Garden 2");
+        source = GetComponent<AudioSource>();
+ 
+        if (PlayerPrefs.GetInt("sceneNo") == 0)
+        {
+            StartCoroutine(Scene1Events());
+        }
+        else if (PlayerPrefs.GetInt("sceneNo") == 1)
+        {
+            yield return StartCoroutine(Scene3Events());  
+            yield return StartCoroutine(Scene4Events()); 
+        }
+        PlayerPrefs.DeleteAll();
+
+    }
+
+    IEnumerator Scene1Events()
+    {
+        foreach (AudioClip clip in scene1Audio)
+        {
+            yield return StartCoroutine(playDialogue(clip));
+        }
+
+        float timer = waitTime;
+        Color c = overlayImage.color;
+        overlayImage.enabled = true;
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            overlayImage.color = new Color(c.r, c.b, c.g, 1f - timer / waitTime);
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        SceneManager.LoadScene("garden_dream");
+        yield return null;
+    }
+
+    IEnumerator Scene3Events()
+    {
+        Debug.Log("Scene 3 Events coroutine");
+        //animation - amanda plants
+        yield return StartCoroutine(playDialogue(scene3Audio[0]));
+        //animation - flower grows
+        yield return StartCoroutine(playDialogue(scene3Audio[1]));
+        //animation - wind blows
+        yield return StartCoroutine(playDialogue(scene3Audio[2]));
+        yield return null;
+    }
+
+    IEnumerator Scene4Events()
+    {
+        Debug.Log("Scene 4 Events coroutine");
+        //animation - amanda next to door, door opens
+        yield return StartCoroutine(playDialogue(scene4Audio[0]));
+        //animation - reaches out to touch player, rain shield
+        yield return StartCoroutine(playDialogue(scene4Audio[1]));
+        //pause
+        yield return StartCoroutine(playDialogue(scene4Audio[2]));
+        yield return null;
+    }
+
+    IEnumerator playDialogue(AudioClip clip)
+    {
+        Debug.Log(clip.name);
+        source.PlayOneShot(clip);
+        yield return new WaitForSeconds(clip.length+1);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
