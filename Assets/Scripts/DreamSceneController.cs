628be90becc89334a6287aadf9b35cc4c5f9064e
Archana <archana.sk19@gmail.com>
2018-06-10 20:52:31 -0700

    completed dream sequence

--- Assets/Scripts/DreamSceneController.cs
+++ Assets/Scripts/DreamSceneController.cs
@@ -12,11 +12,21 @@ public class DreamSceneController : MonoBehaviour {
     public AudioClip[] scene2p4Audio;
 
     private List<GameObject> dadObj;
+    private List<GameObject> amandaObj;
 
     public float delay;
     public float waitTime;
     public float transitionTime;
     public Image overlayImage;
+
+    private GameObject cameraObj;
+    private Vector3 rainCamPosition = new Vector3(294.35f, 9.78f, 380.39f);
+    private float rainCamY = -26.9f;
+    private Vector3 gardenCam = new Vector3(0, 2, 0);
+    private float gardenCamY = -45;
+    private Vector3 flowerCam = new Vector3(-207.78f, 86.83f, 321.28f);
+    private float flowerCamY = 59.7f;
+
     // Use this for initialization
 
     private void Awake()
@@ -25,12 +35,14 @@ public class DreamSceneController : MonoBehaviour {
         scene2p2Audio = Resources.LoadAll<AudioClip>("Audio/Recordings/scene2p2");
         scene2p3Audio = Resources.LoadAll<AudioClip>("Audio/Recordings/scene2p3");
         scene2p4Audio = Resources.LoadAll<AudioClip>("Audio/Recordings/scene2p4");
+        
 
-    }
+}
 
     void Start () {
         DontDestroyOnLoad(gameObject);
-    }
+        
+}
     private void OnEnable() {
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
@@ -42,9 +54,11 @@ public class DreamSceneController : MonoBehaviour {
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
         if (scene.name == "garden_dream") {
             overlayImage = GameObject.Find("Canvas/Overlay").GetComponent<Image>();
+            cameraObj = GameObject.Find("OVRCameraRig");
             StartCoroutine(LoadAnimations(scene));
             Debug.Log("Loaded Garden Dream");
             source = GetComponent<AudioSource>();
+            
             StartCoroutine(IntroFade());
             if (PlayerPrefs.GetInt("sceneNo") == 0) {
                 PlayerPrefs.SetInt("sceneNo", 1);
@@ -56,30 +70,49 @@ public class DreamSceneController : MonoBehaviour {
     IEnumerator Scene2p1Events()
     {
         Debug.Log("scene2p1 Events");
-        foreach (AudioClip clip in scene2p1Audio)
+
+        for (int i = 0;i<3;i++)
         {
-            yield return StartCoroutine(PlayDialogue(clip));
+            yield return StartCoroutine(PlayDialogue(scene2p1Audio[i]));
         }
-        //yield return new WaitUntil(()=> ControllerInputHandler.instance.speed < 0.01f);
-        yield return StartCoroutine(Scene2p2Events()); ;
+        float timer = waitTime;
+        Color c = overlayImage.color;
+        overlayImage.enabled = true;
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            overlayImage.color = new Color(c.r, c.b, c.g, 1f - timer / waitTime);
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        for (int i = 3; i < 6; i++)
+        {
+            yield return StartCoroutine(PlayDialogue(scene2p1Audio[i]));
+        }
+        yield return new WaitUntil(()=> ControllerInputHandler.instance.speed < 0.01f);
+        yield return StartCoroutine(Scene2p2Events()); 
     }
 
     IEnumerator Scene2p2Events()
     {
+        StartCoroutine(MoveCamera(flowerCam, flowerCamY));
         Debug.Log("scene2p2 Events");
         foreach (AudioClip clip in scene2p2Audio)
         {
             yield return StartCoroutine(PlayDialogue(clip));
         }
-        //yield return new WaitUntil(() => ControllerInputHandler.instance.speed > 0.99f);
+        yield return new WaitUntil(() => ControllerInputHandler.instance.speed > 0.99f);
         yield return StartCoroutine(Scene2p3Events());
     }
 
     IEnumerator Scene2p3Events()
     {
+        yield return StartCoroutine(MoveCamera(gardenCam, gardenCamY));
         Debug.Log("scene2p3 Events");
         //amanda and father watching stars
-        dadObj[0].SetActive(true);
+        dadObj[0].SetActive(false);
+        amandaObj[0].SetActive(false);
+        dadObj[1].SetActive(true);
+        amandaObj[1].SetActive(true);
         foreach (AudioClip clip in scene2p3Audio)
         {
             yield return StartCoroutine(PlayDialogue(clip));
@@ -89,8 +122,10 @@ public class DreamSceneController : MonoBehaviour {
 
     IEnumerator Scene2p4Events()
     {
-        dadObj[0].SetActive(false);
-        dadObj[1].SetActive(true);
+        dadObj[1].SetActive(false);
+        dadObj[2].SetActive(true);
+        amandaObj[1].SetActive(false);
+        amandaObj[0].SetActive(true);
         Debug.Log("scene2p4 Events");
         yield return StartCoroutine(PlayDialogue(scene2p4Audio[0]));
         if (ControllerInputHandler.instance.speed<0.5)
@@ -146,10 +181,15 @@ public class DreamSceneController : MonoBehaviour {
     {
         yield return new WaitForEndOfFrame();
         dadObj = new List<GameObject>();
-        dadObj.Add(GameObject.Find("Dad"));
-        dadObj.Add(GameObject.Find("Dadread"));
-        dadObj[0].SetActive(false);
+        amandaObj = new List<GameObject>();
+        dadObj.Add(GameObject.Find("dad"));
+        dadObj.Add(GameObject.Find("dadstars"));
+        dadObj.Add(GameObject.Find("dadread"));
         dadObj[1].SetActive(false);
+        dadObj[2].SetActive(false);
+        amandaObj.Add(GameObject.Find("amandaplain"));
+        amandaObj.Add(GameObject.Find("amandastars"));
+        amandaObj[1].SetActive(false);
     }
 
     private void OnApplicationQuit()
@@ -157,6 +197,24 @@ public class DreamSceneController : MonoBehaviour {
         Debug.Log("player pref reset");
         PlayerPrefs.DeleteAll();
     }
+
+    IEnumerator MoveCamera(Vector3 dest, float destY)
+    {
+        Debug.Log("Move camera");
+        float timer = waitTime;
+        Color c = overlayImage.color;
+        overlayImage.enabled = true;
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            overlayImage.color = new Color(c.r, c.b, c.g, 1f - timer / waitTime);
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        cameraObj.transform.position = dest;
+        cameraObj.transform.eulerAngles = new Vector3(0,destY,0);
+        yield return StartCoroutine(IntroFade());
+    }
+
     IEnumerator IntroFade() {
         float timer = 0;
         Color c = overlayImage.color;
